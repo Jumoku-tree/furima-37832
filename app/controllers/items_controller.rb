cd1c359f05de7c5038d2836bc501ea7a81517d0a
Jumoku-tree <102124101+Jumoku-tree@users.noreply.github.com>
2022-04-22 20:47:36 +0900

    pull request1回目修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :set_item, only: [:show, :edit, :update]
-  before_action :authenticate_user!, only: :edit
+  before_action :authenticate_user!, only: [:edit, :destroy]
 
   def index
     @items = Item.all.order('created_at DESC')
@@ -27,7 +27,9 @@ class ItemsController < ApplicationController
   end
 
   def edit
-    redirect_to new_user_session_path unless user_signed_in? && current_user.id == @item.user_id
+    unless current_user.id == @item.user_id
+      redirect_to root_path
+    end
   end
 
   def update
