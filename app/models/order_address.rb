9abbdd897a7af75a9eb1d4c8e9b885501dc068b1
Jumoku-tree <102124101+Jumoku-tree@users.noreply.github.com>
2022-04-27 16:22:47 +0900

    商品購入機能単体テスト

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -9,7 +9,7 @@ class OrderAddress
     validates :municipality
     validates :token
     validates :postal_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: "must include hyphen(-)" }
-    validates :phone_number, format: { with: /\A[0-9]{10,11}\z/, message: "must be without hypen(-)"}
+    validates :phone_number, format: { with: /\A[0-9]{10,11}\z/, message: "must be without hypen(-) and 10/11 characters"}
     validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
   end
 
---
+++ spec/system/users_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+RSpec.describe "Users", type: :system do
+  before do
+    @user = FactoryBot.build(:user)
+  end
+  describe 'ユーザー新規登録ができる時' do
+    
+  end
+  describe 'ユーザー新規登録ができない時' do
+    
+  end
+end
